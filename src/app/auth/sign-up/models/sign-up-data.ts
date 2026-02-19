d776e3111b939af12868eb857c327ff18e012b94
wiphy <lm802085@gmail.com>
2026-02-19 20:16:50 +0100

    feat: add signal form for sign up page

---
+++ src/app/auth/sign-up/models/sign-up-data.ts
@@ -0,0 +1,5 @@
+export interface SignUpData {
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
--- src/app/auth/sign-up/sign-up.ts
+++ src/app/auth/sign-up/sign-up.ts
@@ -1,9 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
+import { SignUpData } from './models/sign-up-data';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [FormField],
   templateUrl: './sign-up.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignUp {}
+export class SignUp {
+  private readonly signUpModel = signal<SignUpData>({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  readonly signUpForm = form(this.signUpModel);
+
+  readonly isPasswordVisible = signal(false);
+  readonly isConfirmPasswordVisible = signal(false);
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible.update((visible) => !visible);
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.isConfirmPasswordVisible.update((visible) => !visible);
+  }
+}
