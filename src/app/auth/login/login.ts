ec21887525488d3f84ce62db28b903cc2ccaaefd
wiphy <lm802085@gmail.com>
2026-02-20 18:59:48 +0100

    feat: enhance login form with validation and error handling

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,5 +1,14 @@
-import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  email,
+  form,
+  FormField,
+  maxLength,
+  minLength,
+  pattern,
+  required,
+  submit,
+} from '@angular/forms/signals';
 import { LoginData } from './models/login-data';
 
 @Component({
@@ -9,16 +18,40 @@ import { LoginData } from './models/login-data';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Login {
-  readonly loginModel = signal<LoginData>({
+  private readonly loginModel = signal<LoginData>({
     email: '',
     password: '',
   });
 
-  readonly loginForm = form(this.loginModel);
+  readonly loginForm = form(this.loginModel, (schemaPath) => {
+    required(schemaPath.email, { message: '- Email requis' });
+    email(schemaPath.email, { message: "- L'email doit être valide" });
+
+    required(schemaPath.password, { message: '- Mot de passe requis' });
+    minLength(schemaPath.password, 8, {
+      message: '- Minimum 8 caractères',
+    });
+    maxLength(schemaPath.password, 50, {
+      message: '- Maximum 50 caractères',
+    });
+    pattern(schemaPath.password, /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).*$/, {
+      message:
+        '- Minimum une lettre majuscule, minuscule, un chiffre et un caractère spécial (#?!@$%^&*-)',
+    });
+  });
 
   readonly isPasswordVisible = signal(false);
 
   togglePasswordVisibility() {
     this.isPasswordVisible.update((visible) => !visible);
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    submit(this.loginForm, async () => {
+      if (this.loginForm().invalid()) return;
+      console.log(this.loginForm().value());
+    });
   }
 }
